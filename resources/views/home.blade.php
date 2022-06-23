0bfe6640d56f7331aac63285dbe06c66dbb25cb3
destakasep <ferdianfajar543@gmail.com>
2022-06-23 11:15:21 +0700

    commit1st

---
+++ resources/views/home.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@800&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;800&display=swap" rel="stylesheet">
+    <style>
+        body{
+            background-color: #06283D;
+        }
+        .hero{
+            font-family: 'Poppins', sans-serif;;
+            color: #1f1f1f;
+            margin-top: 5%;
+            background-color: #FFE6E6;
+            width: 500px;
+            border-radius: 20px;
+        }
+        .content{
+            width: 30%;
+            margin: auto;
+            margin-top: 30px;
+            box-shadow: 0 10px 10px black;
+            background-color: #ffffff;    
+            border-radius: 50px;
+            font-family: 'Poppins';
+        }
+
+        h4 {
+            font-family: "Poppins";
+            background-color: #C6DCE4;
+            color: #1f1f1f;
+            margin-top: 2%;
+            width: 500px;
+            border-radius: 10px;
+
+        }
+        h5 {
+            font-family: "Poppins";
+        }
+
+        .footer{
+            background-color: #1f1f1f;
+            height: 100px;
+        }
+
+        /* .cardd{
+            display: flex;
+        } */
+    </style>
+</head>
+<body>
+    <center>
+        <h1 class="hero p-3">Biodata Kami</h1>
+        <h4 class="p-4">Kelompok kami terdiri dari Dhifa Martin Nagie dan Desta Fajar Ferdian</h4>
+    </center>
+    <div class="cardd">
+    <div class="content p-3 text-center">
+        <h3>Nama    :Desta Fajar Ferdian</h3>
+        <h3>Jenis kelamin   : Laki-laki</h3>
+        <h3>TTL     : Garut, 29 Desember 2003</h3>
+        <h3>Hobi    : Game, Olahraga</h3>
+        <h3>Jurusan : Sistem Informasi Jaringan dan Aplikasi</h3>
+    </div>
+</div>
+        
+    <div class="content p-3 text-center mb-5">
+        <h3>Nama    :Dhifa Martin Nagie</h3>
+        <h3>Jenis kelamin   : Laki-laki</h3>
+        <h3>TTL     : Garut, 25 Maret 2004</h3>
+        <h3>Hobi    : Riding, Motor</h3>
+        <h3>Jurusan : Sistem Informasi Jaringan dan Aplikasi</h3>
+    </div>
+
+        <center class="p-3">
+            <h4 class="p-3">Dibuat dengan laravel, HTML dan Bootstrap</h4>
+        </center>
+
+    <div class="footer text-center text-light p-5">
+        <p>Thank you For mr Andri for teaching us up on 12th class, we're sorry for made mr Andri upset</p>
+    </div>
+
+        
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+</html>
